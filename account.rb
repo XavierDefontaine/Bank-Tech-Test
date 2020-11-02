44504eb140dc80e3d5dfe5d6d75fa2302b970271
Xavier Defontaine <xdefontaine@gmail.com>
2020-11-02 19:44:01 +0000

    Printable table done

--- account.rb
+++ account.rb
@@ -21,11 +21,19 @@ class Account
   end
 
   def headers
-    "date || credit || debit || balance" 
+    "date       || credit  || debit  || balance\n" 
+  end
+
+  def formatter
+  statement.to_a.reverse.map {|trx| trx[0] + " || "  + 
+  if trx[1][0] < 0 
+    "      " + " || "  + sprintf('%.2f', trx[1][0].abs).to_s + " || "
+  else sprintf('%.2f', trx[1][0].abs).to_s + " || " + "      " + " || "
+  end + sprintf('%.2f', trx[1][1].abs).to_s
+  }.join("\n")
   end
 
   def print
-  statement.to_a.reverse.map {|trx| trx[0] + " || "  + sprintf('%.2f', trx[1][0].abs).to_s + " || " + "      " + " || " + sprintf('%.2f', trx[1][1].abs).to_s
-  }.join("\n")
+   return headers + formatter
   end
 end
