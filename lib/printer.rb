78d91cb02f9d7954ba7a0d6a86542685aaa9ed44
Xavier Defontaine <xdefontaine@gmail.com>
2020-11-03 12:27:09 +0000

    Moved app file to lib dir

---
+++ lib/printer.rb
@@ -0,0 +1,23 @@
+
+module Printer
+
+  def print_statement
+    headers + formatter
+  end
+
+private 
+
+  def headers
+    "date       || credit  || debit  || balance\n"
+  end
+
+  def formatter
+    statement.reverse.map do |trx|
+      trx[:date] + ' || ' +
+        if trx[:credit].nil?
+          "       || #{format('%.2f', trx[:debit].abs)} || "
+        else "#{format('%.2f', trx[:credit].abs)} ||        || "
+        end + format('%.2f', trx[:balance].abs).to_s
+    end.join("\n")
+  end
+end
